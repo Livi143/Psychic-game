943885a65e48bded560343a9f5ae948fc7084e3b
Olivia Fontanese <olfo4481@colorado.edu>
2018-10-11 13:11:50 -0600

    ruby blue colors don't work?

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,3 +8,8 @@ var letters= [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "y", "x", "z"
 ]
 
+function name(){
+    console.log("hello")
+}
+
+name()
