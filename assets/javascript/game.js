465f149aa5de1d578d640930f69570d1398e3eb0
Olivia Fontanese <olfo4481@colorado.edu>
2018-10-10 18:16:25 -0600

    actually initialied the js variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,10 @@
-var numWins;
-var numLosses;
-var numGuessesLeft;
-var yourGuesses;
+var numWins=0;
+var numLosses=0;
+var numGuessesLeft=0;
+var lettersGuessed=[];
+var guessesAvailable=5;
+var computerChoice= "";
+var letters= [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "y", "x", "z"
+]
+
